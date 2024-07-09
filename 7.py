1d8011e18d0449d96d4a4bfaa6f2431eea7bfe0b
Navya Srivastava <93435774+SrivastavaNavya@users.noreply.github.com>
2024-07-09 15:31:22 +0530

    Update 7.py

--- 7.py
+++ 7.py
@@ -1,19 +1,16 @@
 # 7.
+from typing import List
 class Solution:
-    def maxScore(self, cardPoints:List[int],k:int)->:
-        sum_left, sum_right=sum(cardPoints[:k]), 0
-        res=sum_left
+    def maxScore(self,cp: List[int], k: int)->int:
+        tot_sum,subArrSum=sum(cp),sum(cp[:-k])
+        ans=tot_sum-subArrSum
         for i in range(k):
-            sum_lefy-=cardPoints[k-1-i]
-            sum_right+=cardPoints[len(cardPoints)-1-i]
-            res=max(res,sum_left+sum_right)
-        return res
-def main():
-    solution=Solution()
-    cardPoints=[1,2,3,4,5,6,1]
-    k=3
-    result=solution.maxScore(cardPoints,k)
-    print(f"Maximum score: {result}")
+            subArrSum=subArrSum+cp[len(cp)-k+1]-cp[i]
+            ans=max(ans,tot_sum-subArrSum)
+        return ans
     
 if _name_=="_main_":
-    main()
+    solution:Solution()
+    cp=[1,2,3,4,5,6,1]
+    k=3
+    print(solution.maxScore(cp,k))
